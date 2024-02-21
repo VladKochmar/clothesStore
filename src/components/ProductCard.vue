0294e1cd4a41fcd38120d2c981495c803fd5b2c0
Vlad Kochmar <vladkochmar17@gmail.com>
2024-02-21 15:42:19 +0100

    Adaptive and properties were added to the card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="product-card">
-    <div class="product-card__header mb-6">
+    <div class="product-card__header mb-2 mb-lg-6">
       <router-link :to="{ name: 'home' }" class="product-card__img">
-        <img src="https://content.rozetka.com.ua/goods/images/big_tile/396786021.jpg" alt="" />
+        <img :src="productData.imgSrc" :alt="productData.title" />
       </router-link>
       <v-btn
         icon="far fa-heart"
@@ -11,27 +11,43 @@
         size="small"
         class="product-card__btn"
       ></v-btn>
-      <div class="product-card__info">3 for $30</div>
+      <div v-if="hasLabel" class="product-card__label">{{ productData.label }}</div>
     </div>
     <div class="product-card__body">
       <router-link :to="{ name: 'home' }" class="product-card__title mb-2">
-        Stefi Push Up BH schwarz
+        {{ productData.title }}
       </router-link>
-      <div class="product-card__price d-flex ga-2">
-        <span class="product-card__old">80,00€</span><span class="product-card__new">69,00€</span>
+      <div class="product-card__info d-flex ga-2">
+        <span class="product-card__price" :class="{ 'line-through': hasDiscount }">{{
+          productData.price
+        }}</span>
+        <span v-if="hasDiscount" class="product-card__discount">{{ productData.discount }}</span>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  productData: {
+    type: Object,
+    required: true
+  }
+})
+
+const hasDiscount = computed(() => props.productData.discount)
+const hasLabel = computed(() => props.productData.label)
+</script>
 
 <style lang="scss" scoped>
 .product-card {
-  max-width: 390px;
+  max-width: 24.375rem;
   // .product-card__header
   &__header {
     position: relative;
+    background-color: #fafaf9;
   }
   // .product-card__img
   &__img {
@@ -55,8 +71,8 @@
     right: 1.5rem;
     background-color: #fff;
   }
-  // .product-card__info
-  &__info {
+  // .product-card__label
+  &__label {
     position: absolute;
     left: 0;
     top: 1.5rem;
@@ -76,19 +92,27 @@
     line-height: 145.454545%;
     font-family: Inconsolata;
     text-transform: uppercase;
+    @media (max-width: 424.98px) {
+      font-size: 0.875rem;
+    }
+  }
+  // .product-card__info
+  &__info {
+    font-weight: 700;
+    line-height: 137.5%;
+    @media (max-width: 424.98px) {
+      font-size: 0.875rem;
+    }
   }
   // .product-card__price
   &__price {
     color: #828282;
-    font-weight: 700;
-    line-height: 137.5%;
+    &.line-through {
+      text-decoration: line-through;
+    }
   }
-  // .product-card__old
-  &__old {
-    text-decoration: line-through;
-  }
-  // .product-card__new
-  &__new {
+  // .product-card__discount
+  &__discount {
     color: #bd3735;
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,15 @@ import MainMasterPage from '@/masterpages/MainMasterPage.vue'
 
 <template>
   <main-master-page>
-    <product-card />
+    <product-card
+      :product-data="{
+        id: 1,
+        title: 'STEFI PUSH UP BH SCHWARZ',
+        price: '80,00€',
+        discount: '60,00€',
+        label: '3 for $30',
+        imgSrc: 'https://content.rozetka.com.ua/goods/images/big_tile/300057536.jpg'
+      }"
+    />
   </main-master-page>
 </template>
