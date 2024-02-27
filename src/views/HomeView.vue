3105ea53facfa008160d15e6f0865389753d4929
Vlad Kochmar <vladkochmar17@gmail.com>
2024-02-27 11:45:47 +0100

    Our Products section was added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,10 +13,22 @@ onMounted(() => {
 
 <template>
   <main-master-page>
-    <div class="p">
-      <div class="p__container">
-        <products-slider :products-list="catalogStore.getItemsList" />
-      </div>
+    <div class="home">
+      <section class="our-products">
+        <div class="our-products__container">
+          <h2 class="title mb-6 mb-lg-9">{{ $t('pages.home.our-products') }}</h2>
+          <products-slider :products-list="catalogStore.getItemsList" />
+        </div>
+      </section>
     </div>
   </main-master-page>
 </template>
+
+<style lang="scss" scoped>
+.our-products {
+  padding: 6.875rem 0 9.375rem;
+  @media (width <= 1024px) {
+    padding: 50px 0;
+  }
+}
+</style>
